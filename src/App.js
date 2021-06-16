66fb20c2b9f0caf660b012ce102dcb8f212d84e9
Navneeta <navneetaprasad1812@gmail.com>
2021-06-16 20:28:32 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [todoList, settodoList] = useState([])
+
+  const [todo, settodo] = useState('')
+ 
+ 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
+        <input type="text" value={todo} placeholder='Add your Todo here' onChange={(e)=>settodo(e.target.value)} />
+      
+      <br/>
+    
+      <button
+      onClick={()=>{
+        if(todo.length >0){
+          todoList.push(todo )
+          settodoList(todoList)
+          settodo('')
+        }else{
+          alert('Please enter something!')
+        }
+      }}>Add TODO</button>
+
+  
+      
+      {
+        todoList.map((item) =>(
+          <h2>{item}</h2>
+        ))
+      }
+
+
+      
+     
+     
+
     </div>
-  );
-}
+  );}
 
 export default App;
